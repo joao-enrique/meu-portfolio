386c29b66f3f2eb04901f06bc1e47b8e021d3743
joao-enrique <almeidadesousajoaoenrique@gmail.com>
2025-11-10 10:46:20 -0300

    💾 Feat: add scroll reveal

--- js/script.js
+++ js/script.js
@@ -44,3 +44,16 @@ darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('fa-moon');
     document.body.classList.toggle('dark-mode')
 };
+
+/*======================== scroll reveal ================================*/
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 200,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-img img, .services-container, portfolio-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left'});
+ScrollReveal().reveal('.home-content h3, home-content p, .about-content', { origin: 'right'});
